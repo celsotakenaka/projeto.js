c7f1e4226ad6ba6bded0359a873916327cb9fe07
Celso <celso.takenaka@gmail.com>
2024-09-02 02:33:01 -0300

    up p5js todos

--- sketch.js
+++ sketch.js
@@ -1,14 +1,14 @@
 //variáveis da bolinha
-let xBolinha = 300;
+let xBolinha = 40;
 let yBolinha = 200;
-let diametro = 20;
+let diametro = 15;
 let raio = diametro / 2;
 
 //velocidade da bolinha
-let velocidadeXBolinha = 5;
-let velocidadeYBolinha = 5;
+let velocidadeXBolinha = 6;
+let velocidadeYBolinha = 4;
 
-let raqueteComprimento = 10;
+let raqueteComprimento = 8;
 let raqueteAltura = 100;
 
 //variáveis da raquete
@@ -21,13 +21,25 @@ let colidiu = false;
 let meusPontos = 0;
 let pontosOponente = 0;
 
+//sons do jogo
+let raquetada;
+let ponto;
+let trilha;
+
 //variaveis do oponente
 let xRaqueteOponente = 587;
 let yRaqueteOponente = 155;
 let velocidadeYOponente;
 
+function preload(){
+  trilha = loadSound("trilha.mp3");
+  ponto = loadSound("ponto.mp3");
+  raquetada = loadSound("raquetada.mp3");
+}
 function setup() {
   createCanvas(600, 400);
+  trilha.loop();
+  
 }
 
 function draw() {
@@ -81,6 +93,7 @@ function verificaColisaoRaquete() {
     yBolinha + raio > yRaquete
   ) {
     velocidadeXBolinha *= -1;
+    raquetada.play();
   }
 }
 function verificaColisaoRaquete(x, y) {
@@ -95,23 +108,39 @@ function verificaColisaoRaquete(x, y) {
   );
   if (colidiu) {
     velocidadeXBolinha *= -1;
+    raquetada.play();
   }
 }
-function movimentaRaqueteOponente() {
-  velocidadeYOponente =
-    yBolinha - yRaqueteOponente - raqueteComprimento / 2 - 30;
-  yRaqueteOponente += velocidadeYOponente;
+function movimentaRaqueteOponente(){
+  if (keyIsDown(87)){
+    yRaqueteOponente -= 10;
+  }
+  if (keyIsDown(83)) {
+    yRaqueteOponente += 10;
+  }
 }
 function incluiPlacar(){
+  textAlign(CENTER);
+  textSize(16);
+  fill("#FF9800");
+  stroke(255)
+  rect(150, 10, 40, 20);
   fill(255);
-  text(meusPontos, 278, 26);
-  text(pontosOponente, 321, 26);
+  text(meusPontos, 170, 26);
+  fill("#FF9800");
+  stroke(255)
+  rect(450, 10, 40, 20);
+  fill(255);
+  text(pontosOponente, 470, 26);
+  
 }
 function marcaPonto(){
-  if (xBolinha > 590){
+  if (xBolinha > 595){
     meusPontos += 1;
+    ponto.play();
   }
   if (xBolinha < 10){
     pontosOponente += 1;
+    ponto.play();
   }
 }
